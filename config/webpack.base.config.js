260cbd68a062e0677b606a0cb94e57222c97f0ce
respectable2007 <zhanghui8876@163.com>
2019-03-12 09:44:52 +0800

    webpack environment

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: './src/index.js',
   output: {
--- config/webpack.pro.config.js
+++ config/webpack.pro.config.js
@@ -1,4 +1,12 @@
 const merge = require('webpack-merge');
 const base = require('../config/webpack.base.config.js');
+// const webpack = require('webpack');
 module.exports = merge(base,{
+  mode: 'production'
+//   ,
+//   plugins:[
+//     new webpack.DefinePlugin({
+//       'process.env.NODE_ENV': JSON.stringify('production')
+//     })
+//   ]
 })
